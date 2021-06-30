01704802cb5caf5d3bfc5b58237b2919b1dcd3e5
Mikaela Duguil <kimmieduggz@gmail.com>
2021-06-30 13:31:02 -0700

    shows song details on selected song

---
+++ src/reducers/index.js
@@ -0,0 +1,31 @@
+import { combineReducers } from 'redux'
+
+const songReducer = () => {
+  return [
+    {
+      title: 'No Scrubs',
+      duration: '4:05'
+    },
+    {
+      title: 'All Star',
+      duration: '3:45'
+    },
+    {
+      title: 'AAA',
+      duration: '1:16'
+    }
+  ]
+}
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload
+  }
+
+  return selectedSong
+}
+
+export default combineReducers({
+  songs: songReducer,
+  selectedSong: selectedSongReducer
+})
